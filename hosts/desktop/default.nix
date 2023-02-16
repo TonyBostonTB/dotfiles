9cd1834c77b48cb2c68adcb514916a355ca4c6e7
Tony Boston <tboston@posteo.net>
2023-02-16 17:51:40 +0100

    update again and again

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -20,19 +20,7 @@
 
 { config, lib, pkgs, modulesPath, ... }:
 
-let
-  zfsRoot.partitionScheme = {
-    biosBoot = "-part5";
-    efiBoot = "-part1";
-    swap = "-part4";
-    bootPool = "-part2";
-    rootPool = "-part3";
-  };
-  zfsRoot.devNodes = "/dev/disk/by-id/"; # MUST have trailing slash! /dev/disk/by-id/
-  zfsRoot.bootDevices = (import ./machine.nix).bootDevices;
-  zfsRoot.mirroredEfi = "/boot/efis/";
-
-in {
+{
   imports =                                               # For now, if applying to other system, swap files
     [(import ./hardware-configuration.nix)] ++            # Current system hardware config @ /etc/nixos/hardware-configuration.nix
     [(import ../../modules/programs/games.nix)] ++        # Gaming
@@ -106,83 +94,4 @@ in {
     }];
 	}; 
 
-  fileSystems = {
-    "/" = {
-      device = "rpool/nixos/root";
-      fsType = "zfs";
-      options = [ "X-mount.mkdir" ];
-    };
-
-    "/home" = {
-      device = "rpool/nixos/home";
-      fsType = "zfs";
-      options = [ "X-mount.mkdir" ];
-    };
-
-    "/var/lib" = {
-      device = "rpool/nixos/var/lib";
-      fsType = "zfs";
-      options = [ "X-mount.mkdir" ];
-    };
-
-    "/var/log" = {
-      device = "rpool/nixos/var/log";
-      fsType = "zfs";
-      options = [ "X-mount.mkdir" ];
-    };
-
-    "/boot" = {
-      device = "bpool/nixos/root";
-      fsType = "zfs";
-      options = [ "X-mount.mkdir" ];
-    };
-  } // (builtins.listToAttrs (map (diskName: {
-    name = zfsRoot.mirroredEfi + diskName + zfsRoot.partitionScheme.efiBoot;
-    value = {
-      device = zfsRoot.devNodes + diskName + zfsRoot.partitionScheme.efiBoot;
-      fsType = "vfat";
-      options = [
-        "x-systemd.idle-timeout=1min"
-        "x-systemd.automount"
-        "noauto"
-        "nofail"
-      ];
-    };
-  }) zfsRoot.bootDevices));
-
-  swapDevices = (map (diskName: {
-    device = zfsRoot.devNodes + diskName + zfsRoot.partitionScheme.swap;
-    discardPolicy = "both";
-    randomEncryption = {
-      enable = true;
-      allowDiscards = true;
-    };
-  }) zfsRoot.bootDevices);
-
-  hardware.cpu.intel.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.cpu.amd.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  boot.supportedFilesystems = [ "zfs" ];
-  networking.hostId = "abcd1234";
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
-  boot.loader.efi.efiSysMountPoint = with builtins;
-    (zfsRoot.mirroredEfi + (head zfsRoot.bootDevices) + zfsRoot.partitionScheme.efiBoot);
-  boot.zfs.devNodes = zfsRoot.devNodes;
-  boot.loader.efi.canTouchEfiVariables = false;
-  boot.loader.generationsDir.copyKernels = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.copyKernels = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.zfsSupport = true;
-  boot.loader.grub.extraInstallCommands = with builtins;
-    (toString (map (diskName:
-      "cp -r " + config.boot.loader.efi.efiSysMountPoint + "/EFI" + " "
-      + zfsRoot.mirroredEfi + diskName + zfsRoot.partitionScheme.efiBoot + "\n")
-      (tail zfsRoot.bootDevices)));
-  boot.loader.grub.devices =
-    (map (diskName: zfsRoot.devNodes + diskName) zfsRoot.bootDevices);
 }
--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -3,7 +3,19 @@
 # to /etc/nixos/configuration.nix instead.
 { config, lib, pkgs, modulesPath, ... }:
 
-{
+let
+  zfsRoot.partitionScheme = {
+    biosBoot = "-part5";
+    efiBoot = "-part1";
+    swap = "-part4";
+    bootPool = "-part2";
+    rootPool = "-part3";
+  };
+  zfsRoot.devNodes = "/dev/disk/by-id/"; # MUST have trailing slash! /dev/disk/by-id/
+  zfsRoot.bootDevices = (import ./machine.nix).bootDevices;
+  zfsRoot.mirroredEfi = "/boot/efis/";
+
+in {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
@@ -13,6 +25,50 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
+  fileSystems = {
+    "/" = {
+      device = "rpool/nixos/root";
+      fsType = "zfs";
+      options = [ "X-mount.mkdir" ];
+    };
+
+    "/home" = {
+      device = "rpool/nixos/home";
+      fsType = "zfs";
+      options = [ "X-mount.mkdir" ];
+    };
+
+    "/var/lib" = {
+      device = "rpool/nixos/var/lib";
+      fsType = "zfs";
+      options = [ "X-mount.mkdir" ];
+    };
+
+    "/var/log" = {
+      device = "rpool/nixos/var/log";
+      fsType = "zfs";
+      options = [ "X-mount.mkdir" ];
+    };
+
+    "/boot" = {
+      device = "bpool/nixos/root";
+      fsType = "zfs";
+      options = [ "X-mount.mkdir" ];
+    };
+  } // (builtins.listToAttrs (map (diskName: {
+    name = zfsRoot.mirroredEfi + diskName + zfsRoot.partitionScheme.efiBoot;
+    value = {
+      device = zfsRoot.devNodes + diskName + zfsRoot.partitionScheme.efiBoot;
+      fsType = "vfat";
+      options = [
+        "x-systemd.idle-timeout=1min"
+        "x-systemd.automount"
+        "noauto"
+        "nofail"
+      ];
+    };
+  }) zfsRoot.bootDevices));
+
   swapDevices = (map (diskName: {
     device = zfsRoot.devNodes + diskName + zfsRoot.partitionScheme.swap;
     discardPolicy = "both";
@@ -21,6 +77,28 @@
       allowDiscards = true;
     };
   }) zfsRoot.bootDevices);
+
+  boot.supportedFilesystems = [ "zfs" ];
+  networking.hostId = "abcd1234";
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.loader.efi.efiSysMountPoint = with builtins;
+    (zfsRoot.mirroredEfi + (head zfsRoot.bootDevices) + zfsRoot.partitionScheme.efiBoot);
+  boot.zfs.devNodes = zfsRoot.devNodes;
+  boot.loader.efi.canTouchEfiVariables = false;
+  boot.loader.generationsDir.copyKernels = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.copyKernels = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.zfsSupport = true;
+  boot.loader.grub.extraInstallCommands = with builtins;
+    (toString (map (diskName:
+      "cp -r " + config.boot.loader.efi.efiSysMountPoint + "/EFI" + " "
+      + zfsRoot.mirroredEfi + diskName + zfsRoot.partitionScheme.efiBoot + "\n")
+      (tail zfsRoot.bootDevices)));
+  boot.loader.grub.devices =
+    (map (diskName: zfsRoot.devNodes + diskName) zfsRoot.bootDevices);
 
   networking.useDHCP = lib.mkDefault true;
 
